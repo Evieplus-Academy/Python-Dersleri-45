e95e12f23c23ec2b116ed7f64625af4782a051d9
Ferhat Mousavi <ferhat.mousavi@gmail.com>
2023-04-06 22:03:21 +0300

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,12 +1,12 @@
 import re
 
-with open("sample.txt", "r", encoding="utf-8") as file:
-    text = file.read()
+with open("sample.txt", "r", encoding="utf-8") as file_object:
+    text = file_object.read()
 
-pattern = re.compile(r'\b[Uu][Ff][Oo]\b')
-text_censored = pattern.sub("SANSÜRLÜ", text)
+pattern = r"\b[uU][fF][oO]\b"
+text_censored = re.sub(pattern, "SANSÜRLÜ", text)
 
-with open("sample_censored.txt", "w", encoding="utf-8") as file:
-    file.write(text_censored)
+with open("sample_censored.txt", "w", encoding="utf-8") as file_object:
+    file_object.write(text_censored)
 
 print("'sample_censored.txt' dosyası oluşturuldu.")
