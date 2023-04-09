b772e4a87ca7c47df7c5a1adef907f355a6abf75
Ferhat Mousavi <ferhat.mousavi@gmail.com>
2023-04-09 23:01:26 +0300

    Update temel_fonksiyonlar.py

--- temel_fonksiyonlar.py
+++ temel_fonksiyonlar.py
@@ -1,16 +1,55 @@
 import re
 
-pattern = "xyz"
-replace_with = "FERHAT"
-text = "xyz abc 123 xyz"
 
-print(text)
-text = re.sub(pattern, replace_with, text)
-print(text)
+pattern = r"abc"
+text = "xyz abc zyx"
 
-pattern = "HAT"
+match = re.search(pattern, text)
+print(match)
+if match:
+    print("buldum", match.span(), match.group(), match.start(), match.end())
+else:
+    print("bulamadım")
+
+pattern = r"abc"
+text = "abc xyz abc zyx"
+
+match = re.match(pattern, text)
+print(match)
+if match:
+    print("buldum", match.span(), match.group(), match.start(), match.end())
+else:
+    print("bulamadım")
+
+pattern = r"abc"
+text = "abc xyz abc zyx"
+
 matches = re.findall(pattern, text)
 print(matches)
+if matches:
+    print("buldum")
+else:
+    print("bulamadım")
 
 
+pattern = r"axbc"
+text = "abc xyz abc zyx axbc"
 
+matches = re.finditer(pattern, text)
+print(matches)
+for match in matches:
+    print(match, match.group())
+
+
+pattern = r"abc"
+text = "abc xyz abc zyx axbc"
+
+new_text = re.sub(pattern, "XXX", text)
+print(new_text)
+
+
+pattern = r"abc"
+text = "abc xyz abc zyx axbc abc"
+
+splited_list = re.split(pattern, text)
+print(splited_list)
