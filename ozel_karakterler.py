03c8e3884f5dd351c21fbc5e98b7efcb13d62fe5
Ferhat Mousavi <ferhat.mousavi@gmail.com>
2023-04-09 23:03:11 +0300

    Update ozel_karakterler.py

--- ozel_karakterler.py
+++ ozel_karakterler.py
@@ -1,15 +1,38 @@
 import re
 
-pattern1 = r"(\d\d\d)*a"
-pattern2 = r"(\d\D\D){1,3}"
-pattern3 = r"(\w\w\w){1,3}"
-pattern4 = r"(\W\W){1,3}"
-pattern5 = r"(\s\s){1,3}"
-pattern6 = r"(\S\S){1,3}"
-text = "1234\t 4567*!@7890a 0124_  _acas"
 
-pattern = r"hello\s+\w+"
-text = "hello world hello   python hello\nferhat"
+pattern = r"\d{10}"
+text = "5336659992 5326659992 5346659992 532"
+
+matches = re.findall(pattern, text)
+print(matches)
+
+pattern = r"\D{10}"
+text = "5336659992 5326659992 5346659992 532 skjdfh!*sd"
+
+matches = re.findall(pattern, text)
+print(matches)
+
+pattern = r"\w{10}"
+text = "5336659992 5326659992 5346659992 532 skjdfh__sd"
+
+matches = re.findall(pattern, text)
+print(matches)
+
+pattern = r"\W{3}"
+text = "5336659992 !!! 5326659992 5346659992_*?* 532 skjdfh__sd"
+
+matches = re.findall(pattern, text)
+print(matches)
+
+pattern = r"\s{3}"
+text = "5336659992 !!!   5326659992\n\n\n5346659992\t\t\t\t*?* 532 skjdfh__sd"
+
+matches = re.findall(pattern, text)
+print(matches)
+
+pattern = r"\S{3,10}"
+text = "5336659992 !!!   5326659992\n\n\n5346659992\t\t\t\t*?* 532 skjdfh__sd"
 
 matches = re.findall(pattern, text)
 print(matches)
