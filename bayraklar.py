a6495826d00060bc496d05730c4749464500d317
Ferhat Mousavi <ferhat.mousavi@gmail.com>
2023-04-09 23:05:04 +0300

    Update bayraklar.py

--- bayraklar.py
+++ bayraklar.py
@@ -1,26 +1,35 @@
 import re
 
 
-pattern = "fda."
-text = """abc xyz ABC 345 abc fda
-abc jhg 456
-abc 890 ujk fda"""
+pattern = r"^hello"
+text = """HeLLo World!
+hello world!
+hEllo world!"""
 
-# matches = re.findall(pattern, text, flags=re.IGNORECASE)
-# matches = re.findall(pattern, text, flags=re.MULTILINE)
-matches = re.findall(pattern, text, flags=re.DOTALL)
+matches = re.findall(pattern, text, flags=re.M | re.I)
 print(matches)
 
+pattern = r"hello.*world"
+text = """hello.world hello
+world hello\nworld hello world"""
+
+matches = re.findall(pattern, text, flags=re.S)
+print(matches)
 
 pattern = r"""
-        \b          # Kelimenin başında olsun
-        \d{1,3}
-        (,\d{3})*
-        \b
+    \b          # kelimenin başında olsun
+    \d{1,3}     # ilk üç rakam
+    (,\d{3})*
+    \b          # kelimenin sonunda olsun
+    """
+text = """
+Ali: 234,789 TL
+Hasan: 345,980 TL
+Mahmut: 12,000,000 TL
+Semih: 1,655,000 TL
+Merve: 345,765,120 TL
 """
 
-text = "Benim 300,000,000 TL param var. Hasanın 25,000 TL si var."
-
-matches = re.finditer(pattern, text, flags=re.VERBOSE)
+matches = re.finditer(pattern, text, flags=re.X)
 for match in matches:
     print(match.group())
