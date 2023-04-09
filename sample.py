0a0c3faf0df1cf73bb7cf00d1211084b3d554688
Ferhat Mousavi <ferhat.mousavi@gmail.com>
2023-04-09 23:02:39 +0300

    Update sample.py

--- sample.py
+++ sample.py
@@ -10,3 +10,31 @@ with open("sample_censored.txt", "w", encoding="utf-8") as file_object:
     file_object.write(text_censored)
 
 print("'sample_censored.txt' dosyası oluşturuldu.")
+
+def replacement(match):
+    turkish_to_latin = {
+        "ğ": "g",
+        "Ğ": "G",
+        "ı": "i",
+        "İ": "I",
+        "ş": "s",
+        "Ş": "S",
+        "ü": "u",
+        "Ü": "U",
+        "ö": "o",
+        "Ö": "O",
+        "ç": "c",
+        "Ç": "C"
+    }
+    return turkish_to_latin[match.group()]
+
+
+with open("sample.txt", "r", encoding="utf-8") as file_object:
+    text = file_object.read()
+
+pattern = r"[ğĞıİşŞüÜöÖçÇ]"
+new_text = re.sub(pattern, replacement, text)
+
+with open("sample.new.txt", "w", encoding="utf-8") as file_object:
+    file_object.write(new_text)
+    
